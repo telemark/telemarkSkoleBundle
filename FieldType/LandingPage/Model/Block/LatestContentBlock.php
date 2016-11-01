fbf2e519aa53bd9dfd31efaf5c964241e8bc66c9
Messenlien <paal@vangenplotz.no>
2016-11-01 21:21:48 +0100

    namespace fix

--- FieldType/LandingPage/Model/Block/LatestContentBlock.php
+++ FieldType/LandingPage/Model/Block/LatestContentBlock.php
@@ -1,8 +1,4 @@
-    <?php
-/**
- * @copyright Copyright (C) eZ Systems AS. All rights reserved.
- * @license For full copyright and license information view LICENSE file distributed with this source code.
- */
+<?php
 namespace tfk\telemarkSkoleBundle\FieldType\LandingPage\Model\Block;
 
 use eZ\Publish\API\Repository\Values\Content\Query;
